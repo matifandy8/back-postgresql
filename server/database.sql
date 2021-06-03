9564a959e3ac9240a5fff6663a4de820cce5f425
matifandy <matifandy@gmail.com>
2021-06-03 17:54:27 -0300

    db table jobs

--- server/database.sql
+++ server/database.sql
@@ -11,6 +11,13 @@ CREATE TABLE users(
     user_password VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE jobs(
+    job_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    job_description  VARCHAR(255) NOT NULL,
+    job_location  VARCHAR(255) NOT NULL,
+    job_full_time VARCHAR(255) NOT NULL
+);
+
 -- insert fake users
 
-INSERT INTO users (user_name, user_email, user_password) VALUES ('Stenio', 'steniols@gmail.com', 'akjsd1231');
+INSERT INTO jobs (job_description, user_location, user_full_time) VALUES ('React Js', 'Montevideo', 'full time');
