22e2dce092088ebcfbda7b460d37a6bd740c6fb9
matifandy <matifandy@gmail.com>
2021-06-09 12:47:00 -0300

    create job

--- server/controllers/job.js
+++ server/controllers/job.js
@@ -5,39 +5,13 @@ class Job {
     let results = await db.query(`SELECT * FROM jobs`).catch(console.log());
     return results.rows;
   }
-  //create a job.
+  // create a job.
   async createJob(job) {
     await db
       .query(
-        "INSERT INTO jobs (description, location,full_time) VALUES ($1, $2, $3)",
-        [description, location, full_time]
+        "INSERT INTO jobs (job_description,job_location,job_full_time) VALUES ($1,$2,$3)",
+        [job.description, job.location, job.full_time]
       )
-      .catch(console.log);
-    return;
-  }
-
-  //update a job.
-  async updateJob(jobId) {
-    //get the previous job.
-    let original_job = await db
-      .query(`SELECT * FROM jobs WHERE id=$1`, [parseInt(jobId)])
-      .catch(console.log);
-    let new_checked_value = !original_job.rows[0].checked;
-
-    //update the checked job
-    await db
-      .query(`UPDATE jobs SET checked=$1 WHERE id=$2`, [
-        new_checked_value,
-        parseInt(jobId),
-      ])
-      .catch(console.log);
-    return;
-  }
-
-  //delete a job.
-  async deleteJob(jobId) {
-    await db
-      .query(`DELETE FROM jobs WHERE id=$1`, [parseInt(jobId)])
       .catch(console.log);
     return;
   }
--- server/routes/jobs.js
+++ server/routes/jobs.js
@@ -4,31 +4,18 @@ const router = express.Router();
 //import the controller
 const Job = require("../controllers/job");
 
-//Get all todos.
+//Get all jobs.
 router.get("/", async (req, res) => {
   let jobs = await new Job().getJobs();
   console.log(jobs);
   res.json({ jobs });
 });
 
+//Create a job.
 //Create a todo.
 router.post("/job", async (req, res) => {
   let { description, location, full_time } = req.body;
   await new Job().createJob({ description, location, full_time }, res);
-});
-
-//Update a todo.
-router.put("/jobs/:jobId", async (req, res) => {
-  let { jobId } = req.params;
-  await new Job().updateJob(jobId, res);
-  let jobs = await new Job().getJobs();
-});
-
-//Delete a todo.
-router.delete("/jobs/:jobId", async (req, res) => {
-  let { jobId } = req.params;
-  await new Job().deleteJob(jobId);
-  let jobs = await new Job().getJobs();
 });
 
 module.exports = router;
